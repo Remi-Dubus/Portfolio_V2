0a620e42ab808d71bdc7f53358a10c38f4d380ce
REMI DUBUS <remdubus@gmail.com>
2025-05-29 14:39:17 +0200

    feat: header closes #01

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+"use client";
+
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gradient-to-b from-shadow via-dark to-active-dark">
+        <Banner />
     </main>
   );
-}
+};
