f58e26b3dd23cabd41878866c0801a9c020a9f66
REMI DUBUS <remdubus@gmail.com>
2025-06-13 13:58:41 +0200

    feat: add favicon closes #10

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import {LangProvider} from "@/context/LangContext";
 
 export const metadata: Metadata = {
   title: "Portfolio | Rémi Dubus",
-  description: "Junior web developer with skills in HTML, CSS, Typescript, Vite React, Next.js, Node.js, Express & MySQL.",
+  description: "Junior web developer with skills in HTML, CSS, Typescript, Vite, React.js, Next.js, Node.js, Express.js & MySQL.",
 };
 
 export default function RootLayout({
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+        <head>
+            <link rel="icon" href="/favicon.ico" />
+        </head>
         <LangProvider>
             <body
                 className={`${paragraphFont.className} antialiased`}
