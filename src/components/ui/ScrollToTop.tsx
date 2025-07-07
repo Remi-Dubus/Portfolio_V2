24dd8701da3f111802049b8663362ad23b1d4ec8
REMI DUBUS <remdubus@gmail.com>
2025-07-07 18:16:02 +0200

    fix: flicker on scroll on small screen closes #147

--- src/components/ui/ScrollToTop.tsx
+++ src/components/ui/ScrollToTop.tsx
@@ -7,7 +7,7 @@ export default function useScrollToTop() {
     const pathname = usePathname();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
     }, [pathname]);
 
     return null;
